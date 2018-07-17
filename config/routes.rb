fffe2985e92820b66b3a9f8353b2688379603aed
Mohamed Nazim <egynazim@gmail.com>
2018-07-17 19:30:06 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'github/index'
-  root 'github#index'
+  get 'pokemon/index'
+  root 'pokemon#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
