eff99d12d9f76bf300550431bc90eb8559a61b9d
Mohamed Nazim <egynazim@gmail.com>
2018-07-17 18:58:03 -0400

    added model and linked it with controller

--- app/controllers/github_controller.rb
+++
@@ -1,8 +0,0 @@
-class GithubController < ApplicationController
-  def index
-    @profile = HTTParty.get("http://pokeapi.co/api/v2/type/3/").parsed_response
-  end
-end 
-                    
-                     
-                     
